1bdfd714513a778284b6c8b4cb8a23ecefa330ae
Molitor Corentin <corentin.molitor@gmail.com>
2025-01-02 11:05:19 +0100

    Changed approach
    Changed from "Jupyter" to "Readme + supporting scripts"

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # HCL-GWAS
-Ce notebook Jupyter permet de semi-automatiser les études d'associations pangénomiques (GWAS: Genome Wide Assocation Studies), étape par étape.
+
+## Introduction
+
+This readme contains step-by-step instructions to perform a Genome Wide Association Study (GWAS):
+
+- [QC Steps](#gwas-quality-control)
+
+- [GWAS Association](#gwas-association)
+
+- [Downstream Analysis](#downstream-analysis)
+
+## Table of Contents
+
+1. [Introduction](#introduction)
+2. [Table of Contents](#table-of-contents)
+3. [Dependencies](#dependencies)
+3. [The Pipeline](#the-pipeline)
+    - [Input data](#input-data)
+    - [GWAS Quality Control](#gwas-quality-control)
+    - [GWAS association](#gwas-association)
+    - [Downstream analysis](#downstream-analysis)
+
+## Dependencies
+
+ - plink v1.9
+
+ - R
+ 
+ - 
+
+## The Pipeline
+
+Setting up the environment:
+
+```shell
+PLINK="/path/to/plink"
+
+input_folder="./"
+input_basename="Final_dat_merged_corrected"
+out_folder="./"
+
+cd ${out_folder}
+```
+
+### Input data
+
+To run this pipeline you will need genotyping data in the *plink* format:
+ 
+    - [.bed](https://www.cog-genomics.org/plink/1.9/formats#bed): file representing the genotype calls. Must be accompanied by .bim and .fam formats.
+
+    - [.bim](https://www.cog-genomics.org/plink/1.9/formats#bim): file listing the variants (position and alleles).
+  
+    - [.fam](https://www.cog-genomics.org/plink/1.9/formats#fam): file with the samples information (ID, family IDs, sex and phenotype).
+
+More information about the formats can be found in the Plink documentation: https://www.cog-genomics.org/plink/1.9/formats.
+
+### GWAS Quality Control
+
+The first step is to filter individuals and variants with low genotyping rates:
+
+    - Remove individuals with 5% missing genotypes (--mind 0.05)
+
+    - Remove variants with 5% missing genotypes (--geno 0.05)
+
+    - Remove any indivual that must be filtered, for example because of duplicates or removed consent (--remove)
+
+
+
+```shell 
+${PLINK} \
+    --bfile ${input_file} \
+    --geno 0.05 \
+    --mind 0.05 \
+    --remove /path/to/samples_to_exclude.txt \
+    --allow-no-sex \
+    --make-bed \
+    --out ${out_folder}/${input_basename}_QCed
+```
+
+### GWAS association 
+
+### Downstream analysis
+
---
+++ bin/het_check.R
@@ -0,0 +1,86 @@
+# Script to filter individuals with too high or too low heterozygosity (F coeff).
+# Samples are filtered if their F coeff is  or  3 SDs from the population mean.
+
+# The heterozygosity coefficient is obtained with plink --het
+
+# This script outputs two files, one with the valid individuals, that passed the
+# thresholds, the other with the invalid individuals.The name of the output is 
+# based on the input (adding '.valides' and '.nonvalides')
+
+# Dependencies 'optparse'
+if(!require(optparse, quietly = T)) install.packages(optparse) 
+if(!require(ggplot2, quietly = T)) install.packages(ggplot2) 
+
+# Parsing the option from the Rscript command
+option_list <- list(
+    # The .het file from plink
+    make_option(c("-i", "--input"), 
+                type = "character", 
+                default = "", 
+                help = "Full path to plink .het file.",
+                metavar = "character"),
+    
+    # Filtering on F coeff (! low heterozygosity = high F coeff and inversely)
+    make_option(c("-f", "--filter"),
+                type = "character",
+                default = "both",
+                help = "Heterozygosity filter to apply, 'low', 'high' or 'both'",
+                metavar = "character"),
+
+    make_option(c("-v", "--verbose"),
+                type = "logical",
+                default = TRUE,
+                help = "Print information during execution (on by default)")
+);
+
+opt <- parse_args(OptionParser(option_list = option_list))
+
+# Reading the file with the clinical data
+if(file.exists(opt$i)){
+    if(opt$v) print(paste0("Reading .het file", opt$i))
+    het <- read.table(opt$i, header = T)
+} else {
+    stop(paste0("File '", opt$i, "' does not exist."))
+}
+
+# Computing the F coeff mean and sd
+het.moy <- mean(het$F)
+het.sd <- sd(het$F)
+if(opt$v) print(paste0("F coeff mean:  ", het.moy))
+if(opt$v) print(paste0("F coeff SD:    ", het.sd, "\n"))
+
+# ! Heterozygosity and F coeff are inversed
+# F coeff = 1 - (obs het  expected het)
+het$filter <- "valid"
+het$filter[het$F < mean(het$F) + 3*sd(het$F)] <- "low_het"
+het$filter[het$F > mean(het$F) - 3*sd(het$F)] <- "high_het"
+
+# Get the valid and nonvalid samples (3 SDs from the group mean)
+if(opt$f == "high") {
+    valides     <- subset(het, filter == "valid" | filter == "low_het")
+    non.valides <- subset(het, filter == "high_het")
+} else if(opt$f == "low") {
+    valides     <- subset(het, filter == "valid" | filter == "high_het")
+    non.valides <- subset(het, filter == "low_het")
+} else if(opt$f == "both") {
+    valides     <- subset(het, filter == "valid")
+    non.valides <- subset(het, filter == "low_het" | filter == "high_het")
+} else {
+    stop(paste0("Option -f--filter should either 'low', 'high' of 'both', current value '", opt$f, "'"))
+}
+
+# Plot
+png(".plots/PAMPERO_heterozygosity.png", units = in, width = 9, height = 7, res = 350)
+    ggplot(het, aes(x = F, fill = filter)) +
+        geom_histogram(col = black, bins = 50) + 
+        scale_fill_manual(values = c("#009988", "#B2182B", "#4393C3")) + 
+        theme_bw() + ggtitle("GWAS QC - Heterozygosity")
+dev.off()
+
+valides_filename <- paste0(opt$i, '.valides')
+if(opt$v) print(paste0("Writing valid samples to ", valides_filename))
+write.table(x = valides[,c(1,2)], file = valides_filename, quote = F, row.names = F)
+
+nonvalides_filename <- paste0(opt$i, '.nonvalides')
+if(opt$v) print(paste0("Writing nonvalid samples to ", nonvalides_filename))
+write.table(x = non.valides[,c(1,2)], file = nonvalides_filename, quote = F, row.names = F)
