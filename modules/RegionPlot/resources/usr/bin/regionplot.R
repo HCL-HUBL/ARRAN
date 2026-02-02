da8d19a1e3a33791d83f0464f1fc2b51b99d8498
Molitor Corentin <corentin.molitor@chu-lyon.fr>
2026-02-02 13:34:19 +0100

    Changed regionplot.R to have a nicer display of gene names at the bottom ofthe plot

--- modules/RegionPlot/resources/usr/bin/regionplot.R
+++ modules/RegionPlot/resources/usr/bin/regionplot.R
@@ -3,8 +3,8 @@
 # Script for the regional plot, based on the summary statistic file generated with ARRAN
 # But should be compatible with any summary statistic file.
 
-if(!require(optparse, quietly = T)) install.packages(optparse) 
-if(!require(ggplot2, quietly = T)) install.packages(ggplot2) 
+if(!require(optparse, quietly = T)) install.packages(optparse)
+if(!require(ggplot2, quietly = T)) install.packages(ggplot2)
 
 # Parsing the option from the Rscript command
 option_list <- list(
@@ -88,49 +88,51 @@ max_pos <- max(soi$base_pair_location) + win_size/2
 
 # Merging LD info with summary statistics:
 if(opt$v) print("Merging LD and summary statistics")
-summ_stat_ld <- merge(x = summ_stat, y = LD_info[,c("SNP_B", "R2")],
+summ_stat_ld <- merge(x = summ_stat, 
+                      y = LD_info[,c("SNP_B", "R2")],
                       by.x = "rsid", by.y = "SNP_B",
                       all.x = TRUE, all.y = FALSE)
 
 summ_stat_sub <- summ_stat_ld[summ_stat_ld$chromosome == soi$chromosome & 
-                                summ_stat_ld$base_pair_location > min_pos & 
-                                summ_stat_ld$base_pair_location < max_pos,]
+                              summ_stat_ld$base_pair_location > min_pos & 
+                              summ_stat_ld$base_pair_location < max_pos,]
 
 summ_stat_sub$R2[is.na(summ_stat_sub$R2)] <- 0
+
+# Preparing gene data:
+genes_in_region <- genes[genes$chr == soi$chromosome & 
+                         genes$stop > min_pos & 
+                         genes$start < max_pos,]
+genes_in_region <- genes_in_region[order(genes_in_region$start),]
+# Removing coordinates outside the range:
+genes_in_region$start[genes_in_region$start < min_pos] <- min_pos
+genes_in_region$stop[genes_in_region$stop > max_pos] <- max_pos 
+
+# We reserve 20% of the y-axis to plot genes:
+ymax <- max(-log10(summ_stat_sub$p_value))
+ymin <- 0 - ymax/5
+genes_y_pos <- rep(x = c(ymin, ymin/2, ymin/4), times = length(genes_in_region$name))[1:length(genes_in_region$name)]
 
 # Plotting the manhattan plot around the top SNP:
 gg_region <- ggplot(summ_stat_sub, aes(x = base_pair_location, y = -log10(p_value))) + 
                geom_point(data = soi, aes(x = base_pair_location, y = -log10(p_value)), colour = "purple", pch = 18, size = 5) +
-                geom_text(data = soi, aes(x = base_pair_location, y = -log10(p_value), label = rsid), colour = "purple", hjust = -0.3, vjust = -0.1)
-
-# Adding the genes:
-genes_in_region <- genes[genes$chr == soi$chromosome & genes$stop > min_pos & genes$start < max_pos,]
-genes_in_region <- genes_in_region[order(genes_in_region$start),]
-# Removing coordinates outside the range:
-genes_in_region$start[genes_in_region$start < min_pos] <- min_pos
-genes_in_region$stop[genes_in_region$stop > max_pos] <- max_pos
-# To avoid overlapping gene names on the plot:
-vjust_pos <- rep(x = c(-2, 2, -1, 2), times = length(genes_in_region$name))[1:length(genes_in_region$name)] 
-gg_region <- gg_region +
-                geom_segment(data = genes_in_region, aes(x = start, xend = stop, y = 0, yend = 0, colour = name), 
-                             size = 4, alpha = 0.2) +
-                geom_text(data = genes_in_region, aes(x = start, y = 0, label = name, vjust = vjust_pos, colour = name))
-
-# Adjusting labels and theme:
-gg_region <- gg_region + theme_bw() + scale_fill_gradient(low = "#414487", high = "#FDE725FF") + xlab(paste0("chr", soi$chromosome))
-
+               geom_text(data = soi, aes(x = base_pair_location, y = -log10(p_value), label = rsid), colour = "purple", hjust = -0.3, vjust = -0.1) +
+               geom_segment(data = genes_in_region, aes(x = start, xend = stop, y = genes_y_pos, yend = genes_y_pos, colour = name), size = 4, alpha = 0.2) +
+               geom_text(data = genes_in_region, aes(x = start, y = genes_y_pos, label = name, vjust = 1, colour = name)) +
+               theme_bw() + scale_fill_gradient(low = "#414487", high = "#FDE725FF") + xlab(paste0("chr", soi$chromosome)) +
+               ylim(c(ymin, ymax))
+               
 # Plot colored by R2:
-gg_R2 <- gg_region + geom_point(data = summ_stat_sub, aes(fill = R2), pch = 21, size = 3)
+gg_R2 <- gg_region + geom_point(aes(fill = R2), pch = 21, size = 3)
 
-# Plot colored by Effect Size:
-# The column can be "odds_ratio" or "beta" depending on the phenotype.
-# There is probably a more elegant way of dealing with a variable column name with ggplot2: 
+# Plot colored by Effect Size: can be "odds_ratio" or "beta" depending on the phenotype.
 ES_name <- colnames(summ_stat_sub)[6]
-if(ES_name == "odds_ratio") gg_ES <- gg_region + geom_point(data = summ_stat_sub, aes(fill = odds_ratio), pch = 21, size = 3)
-if(ES_name == "beta") gg_ES <- gg_region + geom_point(data = summ_stat_sub, aes(fill = beta), pch = 21, size = 3)
+if(ES_name == "odds_ratio") gg_ES <- gg_region + geom_point(aes(fill = odds_ratio), pch = 21, size = 3)
+if(ES_name == "beta")       gg_ES <- gg_region + geom_point(aes(fill = beta), pch = 21, size = 3)
+# (probably there is a more elegant way of dealing with a variable column name with ggplot2)
 
 if(opt$v) print(paste0("Writing plot to: '", opt$o, "'.pdf"))
 pdf(paste0(opt$o,".pdf"), width = 12)
-     print(gg_R2)
-     print(gg_ES)
+    print(gg_R2)
+    print(gg_ES)
 dev.off()
